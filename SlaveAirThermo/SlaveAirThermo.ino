a8a78810c397b44b28faf3f5f46748c0de475469
Michael Agung <michaelputra.agung@gmail.com>
2021-08-27 10:09:31 +0700

    initial commit

---
+++ SlaveAirThermo/SlaveAirThermo.ino
@@ -0,0 +1,166 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <ArduinoJson.h>
+
+//for mqtt
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme; // I2C
+unsigned long delayTime;
+
+//////////wifi config////////
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+const char* ssid = "myrepublic4";
+const char* password = "komputernet";
+const char* DeviceName= "ThermoHouse";
+const char* mqtt_server = "192.168.0.125";
+int        port     = 1883;
+
+void setup_wifi() {
+
+  delay(1000);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+   
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);    // time to get serial running
+    
+  setup_wifi();
+  client.setServer(mqtt_server, port);
+  client.setCallback(callback);
+
+  
+  Serial.println(F("BME280 test"));
+
+  unsigned status;
+  
+  // default settings
+  status = bme.begin(0x76);  
+  // You can also pass in a Wire library object like &Wire2
+  // status = bme.begin(0x76, &Wire2)
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+      Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+      Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+      Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+      Serial.print("        ID of 0x60 represents a BME 280.\n");
+      Serial.print("        ID of 0x61 represents a BME 680.\n");
+      while (1) delay(10);
+  }
+  
+  Serial.println("-- Default Test --");
+  delayTime = 10*60000;
+
+  Serial.println();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  printValues();
+  delay(delayTime);
+
+}
+void printValues() {
+  DynamicJsonDocument doc(1024);
+  doc["device"] = DeviceName;
+  doc["timestamp"]   = millis();
+
+  doc["data"][0] = bme.readTemperature();
+  doc["data"][1] = (bme.readPressure() / 100.0F);
+  doc["data"][2] = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  doc["data"][3] = bme.readHumidity();
+  char buffer[256];
+  serializeJson(doc, buffer);
+    Serial.print("Temperature = ");
+    Serial.print(doc["data"][0].as<int>());
+    Serial.println(" °C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(doc["data"][1].as<int>());
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(doc["data"][2].as<int>());
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(doc["data"][3].as<int>());
+    Serial.println(" %");
+
+    Serial.println();
+    client.publish("outTopic", buffer);
+}
